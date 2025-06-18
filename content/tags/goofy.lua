47c3a3dcd0ecf2073cb6d5f39eb0e40a1d3f2e4c
Xavier Torrescano <xmtrocks@gmail.com>
2025-06-17 20:09:25 -0700

    make goofy tag's card look right

--- content/tags/goofy.lua
+++ content/tags/goofy.lua
@@ -15,7 +15,13 @@ SMODS.Tag {
         if context.type == self.config.type then
             tag:yep('+', G.C.GREEN, function()
                 local key = 'p_fmod_silly_mega'
-                local card = SMODS.create_card({key = key})
+                local card = Card(
+                    G.play.T.x + G.play.T.w / 2 - G.CARD_W * 1.27 / 2,
+                    G.play.T.y + G.play.T.h / 2 - G.CARD_H * 1.27 / 2,
+                    G.CARD_W * 1.27, G.CARD_H * 1.27, G.P_CARDS.empty,
+                    G.P_CENTERS[key],
+                    { bypass_discovery_center = true, bypass_discovery_ui = true }
+                )
                 card.cost = 0
                 card.from_tag = true
                 G.FUNCS.use_card({config = {ref_table = card}})
