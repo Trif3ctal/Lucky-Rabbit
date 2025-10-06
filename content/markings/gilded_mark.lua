d0af2947819c50691c1d62c45ff6df7ba8ea9664
Xavier Torrescano <xmtrocks@gmail.com>
2025-10-06 14:53:25 -0700

    make gilded cover all use cases (!!!)

--- content/markings/gilded_mark.lua
+++ content/markings/gilded_mark.lua
@@ -4,7 +4,8 @@ LR_UTIL.Marking {
     pos = { x = 1, y = 2 },
     badge_colour = HEX('efbf6f'),
     calculate = function(self, card, context)
-        if context.mod_probability and context.trigger_obj == card then
+        local previous = SMODS.get_previous_context()
+        if context.mod_probability and (context.trigger_obj == card or (previous and previous.other_card == card)) then
             return {
                 numerator = context.numerator * 2
             }
