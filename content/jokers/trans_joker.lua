c992d3617dcbc61b887d3d90a804bd47dda88306
Xavier Torrescano <xmtrocks@gmail.com>
2025-10-07 10:38:30 -0700

    remove trans joker hardcoding

--- content/jokers/trans_joker.lua
+++ content/jokers/trans_joker.lua
@@ -13,6 +13,23 @@ SMODS.Joker {
     rarity = 2,
     unlocked = true,
     discovered = false,
-    blueprint_compat = false,
+    blueprint_compat = true,
     cost = 5,
+    calculate = function(self, card, context)
+        if (context.change_rank and context.old_rank ~= nil) or (context.change_suit and context.old_suit ~= nil) then
+            local bp = context.blueprint_card
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    SMODS.calculate_effect({
+                        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } },
+                        colour = G.C.RED,
+                        message_card = context.other_card,
+                        card = bp or card
+                    })
+                    context.other_card.ability.perma_mult = context.other_card.ability.perma_mult + card.ability.extra.mult
+                    return true
+                end
+            }))
+        end
+    end
 }
