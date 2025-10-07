b6bdf92509f0ca58a96c9a64bf9dbd88dfade3d7
Xavier Torrescano <xmtrocks@gmail.com>
2025-10-07 12:48:34 -0700

    edibles destruction fix

--- content/jokers/edibles.lua
+++ content/jokers/edibles.lua
@@ -65,10 +65,20 @@ SMODS.Joker {
                     end
                 }))
             end
-            if card.ability.extra.uses - 1 <= 0 then
+            if card.ability.extra.uses - card.ability.extra.sub <= 0 then
                 G.E_MANAGER:add_event(Event({
                     func = function()
-                        SMODS.destroy_cards(card, nil, nil, true)
+                        play_sound('tarot1')
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+                            func = function()
+                                    G.jokers:remove_card(card)
+                                    card:remove()
+                                    card = nil
+                                return true; end}))
                         return true
                     end
                 }))
