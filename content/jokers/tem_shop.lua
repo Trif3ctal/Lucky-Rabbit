a27b10e0e8779254c841a4592872e785f908ff2f
Xavier Torrescano <xmtrocks@gmail.com>
2025-10-17 22:39:08 -0700

    fix tem shop not changing prices when obtained

--- content/jokers/tem_shop.lua
+++ content/jokers/tem_shop.lua
@@ -8,29 +8,39 @@ SMODS.Joker {
 	discovered = false,
 	blueprint_compat = false,
     add_to_deck = function(self, card, from_debuff)
-        for i = 1, #G.jokers.cards do
-            G.jokers.cards[i]:set_cost()
-        end
-        for i = 1, #G.consumeables.cards do
-            G.consumeables.cards[i]:set_cost()
-        end
-        if G.shop_jokers then 
-	    for i = 1, #G.shop_jokers.cards do
-                G.shop_jokers.cards[i]:set_cost()
-            end 
-	end
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for i = 1, #G.jokers.cards do
+                    G.jokers.cards[i]:set_cost()
+                end
+                for i = 1, #G.consumeables.cards do
+                    G.consumeables.cards[i]:set_cost()
+                end
+                if G.shop_jokers then
+                    for i = 1, #G.shop_jokers.cards do
+                        G.shop_jokers.cards[i]:set_cost()
+                    end
+                end
+                return true
+            end
+        }))
     end,
     remove_from_deck = function(self, card, from_debuff)
-        for i = 1, #G.jokers.cards do
-            G.jokers.cards[i]:set_cost()
-        end
-        for i = 1, #G.consumeables.cards do
-            G.consumeables.cards[i]:set_cost()
-        end
-        if G.shop_jokers then 
-	    for i = 1, #G.shop_jokers.cards do
-                G.shop_jokers.cards[i]:set_cost()
-            end 
-	end
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for i = 1, #G.jokers.cards do
+                    G.jokers.cards[i]:set_cost()
+                end
+                for i = 1, #G.consumeables.cards do
+                    G.consumeables.cards[i]:set_cost()
+                end
+                if G.shop_jokers then
+                    for i = 1, #G.shop_jokers.cards do
+                        G.shop_jokers.cards[i]:set_cost()
+                    end
+                end
+                return true
+            end
+        }))
     end,
 }
