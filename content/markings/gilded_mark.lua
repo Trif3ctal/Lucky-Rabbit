9d4ed3bed993b104cdd079dadae4b6091c766db6
Xavier Torrescano <xmtrocks@gmail.com>
2025-11-02 12:41:27 -0800

    make gilded use full corners

--- content/markings/gilded_mark.lua
+++ content/markings/gilded_mark.lua
@@ -1,7 +1,7 @@
 LR_UTIL.Marking {
     key = "gilded_mark",
     atlas = "Decks",
-    pos = { x = 1, y = 2 },
+    pos = { x = 0, y = 2 },
     badge_colour = HEX('efbf6f'),
     calculate = function(self, card, context)
         local previous = SMODS.get_previous_context()
