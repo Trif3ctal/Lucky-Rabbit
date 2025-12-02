1db142757e80b5b041dd5762f6dde8e4d8b38732
Xavier Torrescano <xmtrocks@gmail.com>
2025-12-01 19:50:05 -0800

    10 million nil checks

--- content/jokers/tem_shop.lua
+++ content/jokers/tem_shop.lua
@@ -10,13 +10,17 @@ SMODS.Joker {
     add_to_deck = function(self, card, from_debuff)
         G.E_MANAGER:add_event(Event({
             func = function()
-                for i = 1, #G.jokers.cards do
-                    G.jokers.cards[i]:set_cost()
+                if G.jokers and G.jokers.cards then
+                    for i = 1, #G.jokers.cards do
+                        G.jokers.cards[i]:set_cost()
+                    end
                 end
-                for i = 1, #G.consumeables.cards do
-                    G.consumeables.cards[i]:set_cost()
+                if G.consumeables and G.consumeables.cards then
+                    for i = 1, #G.consumeables.cards do
+                        G.consumeables.cards[i]:set_cost()
+                    end
                 end
-                if G.shop_jokers then
+                if G.shop_jokers and G.shop_jokers.cards then
                     for i = 1, #G.shop_jokers.cards do
                         G.shop_jokers.cards[i]:set_cost()
                     end
@@ -28,13 +32,17 @@ SMODS.Joker {
     remove_from_deck = function(self, card, from_debuff)
         G.E_MANAGER:add_event(Event({
             func = function()
-                for i = 1, #G.jokers.cards do
-                    G.jokers.cards[i]:set_cost()
+                if G.jokers and G.jokers.cards then
+                    for i = 1, #G.jokers.cards do
+                        G.jokers.cards[i]:set_cost()
+                    end
                 end
-                for i = 1, #G.consumeables.cards do
-                    G.consumeables.cards[i]:set_cost()
+                if G.consumeables and G.consumeables.cards then
+                    for i = 1, #G.consumeables.cards do
+                        G.consumeables.cards[i]:set_cost()
+                    end
                 end
-                if G.shop_jokers then
+                if G.shop_jokers and G.shop_jokers.cards then
                     for i = 1, #G.shop_jokers.cards do
                         G.shop_jokers.cards[i]:set_cost()
                     end
