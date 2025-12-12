ee7f031ab67121bf1dbfe81b4f8ae1be9bbde344
Xavier Torrescano <xmtrocks@gmail.com>
2025-12-11 17:00:50 -0800

    make tricky always mark 13 cards

--- content/decks/tricky.lua
+++ content/decks/tricky.lua
@@ -14,7 +14,14 @@ SMODS.Back {
         G.E_MANAGER:add_event(Event({
             func = function()
                 for i = 1, self.config.extra.amount do
-                    local card = pseudorandom_element(G.playing_cards, "tricky_" .. i)
+                    local applicable = {}
+                    for b = 1, #G.playing_cards do
+                        local card = G.playing_cards[b]
+                        if not LR_UTIL.has_marking(card) then
+                            applicable[#applicable + 1] = card
+                        end
+                    end
+                    local card = pseudorandom_element(applicable, pseudoseed('tricky_' .. i))
                     LR_UTIL.set_marking(card,
                             string.sub(pseudorandom_element(LR_UTIL.ENABLED_MARKINGS, pseudoseed('tricky_' .. i)), 1, -6))
                 end
