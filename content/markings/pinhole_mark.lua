7de110da5b1e02869918c4fb91056efa1ed5e7f1
Xavier Torrescano <xmtrocks@gmail.com>
2026-04-14 21:50:17 -0700

    update to 1606b

--- content/markings/pinhole_mark.lua
+++ content/markings/pinhole_mark.lua
@@ -3,4 +3,9 @@ LR_UTIL.Marking {
     atlas = "Decks",
     pos = { x = 1, y = 0 },
     badge_colour = HEX('4f6367'),
+    calculate = function(self, card, context)
+        if context.stay_flipped and context.from_area == G.play and context.to_area == G.discard then
+            return { modify = { to_area = G.deck } }
+        end
+    end,
 }
